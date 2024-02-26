a651c07abf7637ed884ece6b4e2958d0e159221c
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-02-26 12:19:44 +0100

    Initialize the repo

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { metadata } from "./layout";
+
+export default function Home() {
+  return (
+    <main className="p-4 h-full flex bg-gray-50">
+      <h1>Image Generator</h1>
+    </main>
+  );
+}
